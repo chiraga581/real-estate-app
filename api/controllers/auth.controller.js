bbdbdc559d6b65322ad78674ce32d073755d1c50
chiraga581 <chiraga581@gmail.com>
2024-01-06 02:52:24 +0530

    complete create listing page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -34,7 +34,7 @@ export const signin = async(req , res , next ) => {
         }
 
         const token = jwt.sign({id : validUser._id},process.env.JWT_SECRET)
-        const {password : pass , ...rest} = validUser._doc
+        const {password : pass , ...rest} = validUser._doc;
         res
             .cookie('access_token' , token , {httpOnly: true})
             .status(200)
@@ -49,7 +49,7 @@ export const  google = async (req,res,next) => {
     try {
         const user = await User.findOne({email : req.body.email });
         if(user){
-            const token = jwt.sign({id: user.id}, process.env.JWT_SECRET);
+            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
             const {password: pass , ...rest} = user._doc;
             res 
                 .cookie('access_token',token , {httpOnly:true})
@@ -65,7 +65,7 @@ export const  google = async (req,res,next) => {
                 avatar : req.body.photo,
             })
             await newUser.save();
-            const token = jwt.sign({id :newUser._id} , process.env.JWT_SECRET);
+            const token = jwt.sign({id : newUser._id} , process.env.JWT_SECRET);
             const { password: pass , ...rest} = newUser._doc;
             res 
                 .cookie('access_token' , token , {httpOnly: true})
